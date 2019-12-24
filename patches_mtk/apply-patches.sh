2ae81e6c43832745523fc31f33a68845532e1b8f
h0sch180 <hoschi@anukis.de>
2019-12-24 15:01:59 +0000

    fix patch apply script

--- patches_mtk/apply-patches.sh
+++ patches_mtk/apply-patches.sh
@@ -17,11 +17,11 @@ cd ..
 cd system/sepolicy
 patch -p1 < ../../device/$brand/$model/patches_mtk/system_sepolicy.patch
 cd ../..
-cd system/netd
-patch -p1 < ../../device/$brand/$model/patches_mtk/system_netd.patch
-cd ../..
 
 ### DO NOT USE SYSTEM BLUETOOTH PATCH
+#cd system/netd
+#patch -p1 < ../../device/$brand/$model/patches_mtk/system_netd.patch
+#cd ../..
 #cd system/bt
 #patch -p1 < ../../device/$brand/$model/patches_mtk/system_bt.patch
 #cd ../..
@@ -35,7 +35,7 @@ patch -p1 < ../../device/$brand/$model/patches_mtk/frameworks_native.patch
 cd ..
 cd base
 patch -p1 < ../../device/$brand/$model/patches_mtk/framework_base/0001-frameworks_base.patch
-patch -p1 < ../../device/$brand/$model/patches_mtk/framework_base/0002-Fix-Minior-Bugs.patch
+patch -p1 -N < ../../device/$brand/$model/patches_mtk/framework_base/0002-Fix-Minior-Bugs.patch
 patch -p1 < ../../device/$brand/$model/patches_mtk/framework_base/0003-frameworks_base_signature_spoofing.patch
 git clean -f -d
 cd ..
